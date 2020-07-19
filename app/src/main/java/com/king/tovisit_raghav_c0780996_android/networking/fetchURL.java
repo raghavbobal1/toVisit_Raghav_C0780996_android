b4370f0a4b5d0837c49fd24bfe3e47d5ba6d0bdc
raghavbobal1 <raghavbobal001@gmail.com>
2020-07-19 18:34:14 -0400

    added fetchURL class

--- app/src/main/java/com/king/tovisit_raghav_c0780996_android/networking/fetchURL.java
+++ app/src/main/java/com/king/tovisit_raghav_c0780996_android/networking/fetchURL.java
@@ -1,4 +1,46 @@
 package com.king.tovisit_raghav_c0780996_android.networking;
+import android.util.Log;
 
-public class fetchURL {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import static android.content.ContentValues.TAG;
+
+public class fetchURL
+{
+    public String readURL(String myUrl) throws IOException {
+
+        Log.i(TAG, "readURL: ");
+
+        String data = "";
+        InputStream inputStream = null;
+
+        HttpURLConnection httpURLConnection = null;
+
+        try {
+            URL url = new URL(myUrl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            inputStream = httpURLConnection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while((line = reader.readLine()) != null)
+                stringBuffer.append(line);
+            data = stringBuffer.toString();
+            reader.close();
+        } catch(MalformedURLException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            inputStream.close();
+            httpURLConnection.disconnect();
+        }
+
+        return data;
+    }
 }
