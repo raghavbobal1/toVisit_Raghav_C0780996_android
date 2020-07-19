b91eb554ebb0d7818cf3673c3759640d765b9359
raghavbobal1 <raghavbobal001@gmail.com>
2020-07-19 18:40:57 -0400

    added GetDirectionsData class

--- app/src/main/java/com/king/tovisit_raghav_c0780996_android/networking/GetDirectionsData.java
+++ app/src/main/java/com/king/tovisit_raghav_c0780996_android/networking/GetDirectionsData.java
@@ -1,4 +1,35 @@
 package com.king.tovisit_raghav_c0780996_android.networking;
 
-public class GetDirectionsData {
+import android.os.AsyncTask;
+import android.util.Log;
+import com.king.tovisit_raghav_c0780996_android.networking.fetchURL;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+public class GetDirectionsData extends AsyncTask<Object, String, String> {
+
+    String directionData, url;
+    GoogleMap mMap;
+    LatLng latLng;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        Log.i(TAG, "doInBackground: GET DIRECTION DATA ");
+        mMap = (GoogleMap) objects[0];
+        url = (String) objects[1];
+        latLng = (LatLng) objects[2];
+
+        fetchURL fetchURL= new fetchURL();
+        try{
+            directionData = fetchURL.readURL(url);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return directionData;
+    }
 }
