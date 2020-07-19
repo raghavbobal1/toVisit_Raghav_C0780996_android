130b5a90b9ae6f673978645219b4f895a3d28154
raghavbobal1 <raghavbobal001@gmail.com>
2020-07-19 18:05:18 -0400

    completed helper class places

--- app/src/main/java/com/king/tovisit_raghav_c0780996_android/helperClass/Places.java
+++ app/src/main/java/com/king/tovisit_raghav_c0780996_android/helperClass/Places.java
@@ -1,4 +1,61 @@
 package com.king.tovisit_raghav_c0780996_android.helperClass;
 
-public class Places {
+import java.io.Serializable;
+
+public class Places implements Serializable
+{
+
+    private String name;
+    private Boolean isVisited;
+    private int id;
+    private Double lat, lng;
+
+    public Places(int id, String name, Boolean isVisited, Double lat, Double lng) {
+        this.name = name;
+        this.isVisited = isVisited;
+        this.lat = lat;
+        this.lng = lng;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(Boolean visited) {
+        isVisited = visited;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
 }
